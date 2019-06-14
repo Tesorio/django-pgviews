02fb2b254d3504def8a44acc05f55af5c820dd5c
Caio Ariede <caio.ariede@gmail.com>
2019-06-14 11:13:00 -0300

    Add PGVIEWS_SYNCER_CLASS setting

--- django_pgviews/apps.py
+++ django_pgviews/apps.py
@@ -2,6 +2,8 @@ import logging
 
 from django import apps
 from django.db.models import signals
+from django.utils.module_loading import import_string
+from django.conf import settings
 
 log = logging.getLogger('django_pgviews.sync_pgviews')
 
@@ -19,14 +21,15 @@ class ViewConfig(apps.AppConfig):
         """
         self.counter = self.counter + 1
         total = len([a for a in apps.apps.get_app_configs() if a.models_module is not None])
-        
+
         if self.counter == total:
             log.info('All applications have migrated, time to sync')
-            # Import here otherwise Django doesn't start properly
-            # (models in app init are not allowed)
-            from .models import ViewSyncer
-            vs = ViewSyncer()
-            vs.run(force=True, update=True) 
+            view_syncer_cls = self.get_view_syncer_class()
+            vs = view_syncer_cls()
+            vs.run(force=True, update=True)
+
+    def get_view_syncer_class(self, default='django_pgviews.models.ViewSyncer'):
+        return import_string(getattr(settings, 'PGVIEWS_SYNCER_CLASS', None) or default)
 
     def ready(self):
         """Find and setup the apps to set the post_migrate hooks for.
